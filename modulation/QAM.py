1f3292a5f2f646efe8bed89d527abb13d4b7ae26
aweiss <alec.weiss@nist.gov>
2019-06-17 13:24:11 -0600

    Constellation separated from modem

--- modulation/QAM.py
+++ modulation/QAM.py
@@ -10,45 +10,12 @@ from Modulation import Modulation
 from Modulation import generate_gray_code_mapping,generate_root_raised_cosine,lowpass_filter
 import numpy as np
 import matplotlib.pyplot as plt
-import random
-import scipy
 import cmath
 
 class QAMModem(Modulation):
     '''
     A class to modulate a set of data using an M-QAM modulation scheme.
     Inherits from the Modulation superclass
-    
-    Example
-    -------
-    Examples can be given using either the ``Example`` or ``Examples``
-    sections. Sections support any reStructuredText formatting, including
-    literal blocks::
-    
-        $ python example_numpy.py
-    
-    
-    Section breaks are created with two blank lines. Section breaks are also
-    implicitly created anytime a new section starts. Section bodies *may* be
-    indented:
-    
-    Notes
-    -----
-        This is an example of an indented section. It's like any other section,
-        but the body is indented to help it stand out from surrounding text.
-    
-    If a section is indented, then a section break is created by
-    resuming unindented text.
-    
-    Attributes
-    ----------
-    module_level_variable1 : int
-        Module level variables may be documented in either the ``Attributes``
-    
-    Methods
-    -------
-    colorspace(c='rgb')
-        Represent the photo in the given colorspace.
     '''
     def __init__(self,M,**arg_options):
         '''
@@ -58,9 +25,7 @@ class QAMModem(Modulation):
         defaults = {} #default options
         arg_options.update(defaults)
         super().__init__(**arg_options)
-        self.M = M #what qam we are using
-        self.constellation_dict = {} #dictionary containing key/values for binary/RI constellation
-        self._generate_mqam_constellation(M)
+        self.constellation = QAMConstellation(M)
         
     ##########################################################################
     # functions for time domain baseband decoding/encoding
@@ -276,28 +241,13 @@ class QAMModem(Modulation):
     ##########################################################################
     # Functions for dealing with the constellation
     ##########################################################################
-    
-    def _generate_mqam_constellation(self,M):
-        '''
-        @brief generate an M-QAM constellation
-        @param[in] M - order of QAM (must be power of 2)
-        '''
-        self.constellation_dict = generate_gray_code_mapping(M,generate_qam_position)
         
     def plot_constellation(self,**arg_options):
         '''
         @brief plot the constellation points defined in constellation_dict
+        @return a figure with the constellation points
         '''
-        #configure the figure
-        fig = plt.figure()
-        ax  = plt.axes()
-        ax.set_xlim([-1.5,1.5])
-        ax.set_ylim([-1.5,1.5])
-        #ax.grid(linestyle='dotted')
-        for k,v in self.constellation_dict.items():
-            plt.plot(v.real,v.imag,'bo')
-            plt.text(v.real,v.imag+0.05,"".join(str(int(x)) for x in k),ha='center')
-        return fig
+        return self.constellation.plot()
     
     def map_to_constellation(self,data,**arg_options):
         '''
@@ -310,26 +260,7 @@ class QAMModem(Modulation):
             bitstream - True if the data is a bitstream (array of 1s and 0s) (default False)
         @return a list of complex numbers for the corresponding mapping, mapped bitstream
         '''
-        options = {}
-        options['padding'] = 0
-        options['bitstream'] = False
-        for key,val in arg_options.items():
-            options[key] = val
-        data = bytearray(data) #to bytearray
-        bitstream = np.unpackbits(data) #to bits
-        mlog2 = np.log2(self.M)
-        mlog2 = int(round(mlog2)) #maybe deal with this in the future
-        len_mod = len(bitstream)%mlog2
-        if len_mod is not 0: #extra bits we need
-            print("Warning bits not divisible by encoding (%d/%d). Padding end with %ds" %(len(bitstream),mlog2,options['padding']))
-            extra_bits = round(mlog2-len_mod)
-            bitstream = np.append(bitstream,np.full((extra_bits,),options['padding']))
-        split_bits = np.split(bitstream,len(bitstream)/mlog2) #split into packets
-        locations = []
-        for pack in split_bits:
-            loc = self.get_constellation_location(pack)
-            locations.append(loc)
-        return np.array(locations),bitstream
+        return self.constellation.map(data,**arg_options)
     
     def unmap_from_constellation(self,locations):
         '''
@@ -337,72 +268,9 @@ class QAMModem(Modulation):
         @param[in] locations - iq values to unmap
         @return a bytearray of the unmapped values, array of bits (bitstream)
         '''
-        vals = self.get_constellation_values(locations)
-        bitstream = vals.reshape((-1,)).astype('int')
-        packed_vals = bytearray(np.packbits(bitstream))
-        return packed_vals,bitstream
+        return self.constellation.unmap(locations)
     
-    def get_constellation_location(self,bits):
-        '''
-        @brief get a location (complex number) on the constellation given
-            a set of bits. These bits should be an array (or list) of ones and
-            zeros in LSB first order (how np.unpackbits provides values)
-        @param[in] bits - list (or array) of bits in LSB first order (usually form np.unpackbits)
-        '''
-        #some checks
-        mlog2 = np.log2(self.M)
-        if len(bits) != mlog2:
-            raise ValueError("Number of bits must equal Log2(M)")
-        if not np.all(np.logical_or(bits==1,bits==0)):
-            raise ValueError("Bits argument must be a list (or array) of only zeros or ones")
-        key = tuple(np.flip(bits,axis=0)) #MSB first for key
-        val = self.constellation_dict[key]
-        return val
-    
-    def get_constellation_values(self,locations):
-        '''
-        @brief get a value (code) from a given constellation location(s) (complex number)
-        @param[in] locations - complex number(s) for constellation value
-        '''
-        if not hasattr(locations,'__iter__'):
-            locations = [locations]
-        location_dict = self.location_constellation_dict
-        values = []
-        for l in locations:
-            val = np.flip(location_dict[l],axis=0)
-            values.append(val)
-        return np.array(values)
-    
-    @property
-    def codes(self):
-        '''
-        @brief getter for the coding of the qam
-        '''
-        codes = []
-        for k,v in self.constellation_dict.items():
-            codes.append(k)
-        return np.array(codes)
-    
-    @property
-    def locations(self):
-        '''
-        @brief getter for the constellation locations (complex numbers)
-        '''
-        locations = []
-        for k,v in self.constellation_dict.items():
-            locations.append(v)
-        return np.array(locations)
-    
-    @property
-    def location_constellation_dict(self):
-        '''
-        @brief this returns a dictionary the same as constellation_dict but with
-        the key/value pairs flipped
-        '''
-        location_map_dict = {}
-        for k,v in self.constellation_dict.items():
-            location_map_dict[v] = k
-        return location_map_dict
+
    
    #def modulate()
    #    pas 
@@ -529,6 +397,147 @@ class QAMCorrection():
            adj_iq = cmath.rect(mag,phase)
            corrected_data[i] = adj_iq
         return corrected_data
+    
+class QAMConstellation():
+    '''
+    @brief class to map, unmap, and hold a qam constellation
+    '''
+    def __init__(self,M,**arg_options):
+        '''
+        @brief initialize the modulation class
+        @param[in/OPT] M - type of QAM (e.g 4,16,256)
+        '''
+        self.M = M
+        self.constellation_dict = {} #dictionary containing key/values for binary/RI constellation
+        self._generate_qam_constellation() #generate the constellation for M-QAM
+        
+    def map(self,data,**arg_options):
+        '''
+        @brief map input data to constellation. If the number of bits does
+        not fit in the encoding then pad with the value given by padding
+        @param[in] data - data to map. currently supports 'uint'
+            for raw bitstream use named argument 'bitstream' to True
+        @param[in/OPT] arg_options - optional keyword arguments as follows:
+            padding - value to pad bits with if encoding doesnt fit (default 0)
+            bitstream - True if the data is a bitstream (array of 1s and 0s) (default False)
+        @return a list of complex numbers for the corresponding mapping, mapped bitstream
+        '''
+        options = {}
+        options['padding'] = 0
+        options['bitstream'] = False
+        for key,val in arg_options.items():
+            options[key] = val
+        data = bytearray(data) #to bytearray
+        bitstream = np.unpackbits(data) #to bits
+        mlog2 = np.log2(self.M)
+        mlog2 = int(round(mlog2)) #maybe deal with this in the future
+        len_mod = len(bitstream)%mlog2
+        if len_mod is not 0: #extra bits we need
+            print("Warning bits not divisible by encoding (%d/%d). Padding end with %ds" %(len(bitstream),mlog2,options['padding']))
+            extra_bits = round(mlog2-len_mod)
+            bitstream = np.append(bitstream,np.full((extra_bits,),options['padding']))
+        split_bits = np.split(bitstream,len(bitstream)/mlog2) #split into packets
+        locations = []
+        for pack in split_bits:
+            loc = self._get_location(pack)
+            locations.append(loc)
+        return np.array(locations),bitstream
+    
+    def unmap(self,locations):
+        '''
+        @brief unmap a set of iq (complex) values from the constellation
+        @param[in] locations - iq locations to unmap to bits
+        @return a bytearray of the unmapped values, array of bits (bitstream)
+        '''
+        vals = self._get_values(locations)
+        bitstream = vals.reshape((-1,)).astype('int')
+        packed_vals = bytearray(np.packbits(bitstream))
+        return packed_vals,bitstream
+    
+    def _get_location(self,bits):
+        '''
+        @brief get a location (complex number) on the constellation given
+            a set of bits. These bits should be an array (or list) of ones and
+            zeros in LSB first order (how np.unpackbits provides values)
+        @param[in] bits - list (or array) of bits in LSB first order (usually form np.unpackbits)
+        '''
+        #some checks
+        mlog2 = np.log2(self.M)
+        if len(bits) != mlog2:
+            raise ValueError("Number of bits must equal Log2(M)")
+        if not np.all(np.logical_or(bits==1,bits==0)):
+            raise ValueError("Bits argument must be a list (or array) of only zeros or ones")
+        key = tuple(np.flip(bits,axis=0)) #MSB first for key
+        val = self.constellation_dict[key]
+        return val
+    
+    def _get_values(self,locations):
+        '''
+        @brief get a value (code) from a given constellation location(s) (complex number)
+        @param[in] locations - complex number(s) for constellation value
+        '''
+        if not hasattr(locations,'__iter__'):
+            locations = [locations]
+        location_dict = self.location_constellation_dict
+        values = []
+        for l in locations:
+            val = np.flip(location_dict[l],axis=0)
+            values.append(val)
+        return np.array(values)
+    
+    def _generate_qam_constellation(self):
+        '''
+        @brief generate an M-QAM constellation
+        @param[in] M - order of QAM (must be power of 2)
+        '''
+        self.constellation_dict = generate_gray_code_mapping(self.M,generate_qam_position)
+    
+    
+    def plot(self,**arg_options):
+        '''
+        @brief plot the constellation points defined in constellation_dict
+        '''
+        #configure the figure
+        fig = plt.figure()
+        ax  = plt.axes()
+        ax.set_xlim([-1.5,1.5])
+        ax.set_ylim([-1.5,1.5])
+        #ax.grid(linestyle='dotted')
+        for k,v in self.constellation_dict.items():
+            plt.plot(v.real,v.imag,'bo')
+            plt.text(v.real,v.imag+0.05,"".join(str(int(x)) for x in k),ha='center')
+        return fig
+    
+    @property
+    def codes(self):
+        '''
+        @brief getter for the coding of the qam
+        '''
+        codes = []
+        for k,v in self.constellation_dict.items():
+            codes.append(k)
+        return np.array(codes)
+    
+    @property
+    def locations(self):
+        '''
+        @brief getter for the constellation locations (complex numbers)
+        '''
+        locations = []
+        for k,v in self.constellation_dict.items():
+            locations.append(v)
+        return np.array(locations)
+    
+    @property
+    def location_constellation_dict(self):
+        '''
+        @brief this returns a dictionary the same as constellation_dict but with
+        the key/value pairs flipped
+        '''
+        location_map_dict = {}
+        for k,v in self.constellation_dict.items():
+            location_map_dict[v] = k
+        return location_map_dict
 
 def generate_qam_position(code_number,num_codes):
     '''
@@ -547,16 +556,14 @@ def generate_qam_position(code_number,num_codes):
 
 import copy
 if __name__=='__main__':
-    
-    #codes = generate_gray_code_mapping(32)
-    #for c in np.flip(codes,axis=1):
-    #    print(c)
+    #q256 = QAMConstellation(16)
+    #q256.plot()
 
-    mymodem = QAMModem(256)
+    mymodem = QAMModem(64)
     #fig=myqm.plot_constellation()
     #mymap,inbits = myq.map_to_constellation(bytearray('testing'.encode()))
     data = 'testing'.encode()
-    data = np.random.random(10)
+    #data = np.random.random(10)
     #data = [255,255,255]
     
     print("Encoding")
@@ -600,6 +607,7 @@ if __name__=='__main__':
     print("Calculating EVM")
     evm = mymodem.calculate_evm(inqam,outqam)
     print(evm)
+
 
     
     
