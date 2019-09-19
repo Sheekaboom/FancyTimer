a5801088ceb40d7b842c5167eec95c98092e8b33
Alec <aweiss7791@gmail.com>
2019-09-19 08:32:40 -0600

    removed, and updated some files

--- beamforming/BeamFort.py
+++ beamforming/BeamFort.py
@@ -38,6 +38,7 @@ def build_fortran_library(file_path,module_name=None,out_subdir='python_bindings
     
 if __name__=='__main__':
     import os
+    import numpy as np
     import ctypes
     wdir = r'C:\Users\aweis\git\pycom\beamforming\fortran\build'
     mod = ctypes.CDLL(os.path.join(wdir,'libbeamform_serial.dll'))
@@ -47,6 +48,81 @@ if __name__=='__main__':
     cb = ctypes.c_float(5.)
     res = mod.__beamforming_serial_MOD_fortran_mult_funct(ctypes.pointer(ca),ctypes.pointer(cb))
     print("{} * {} = {}".format(ca,cb,res))
+
+    #numpy array multiplication in fortran
+    mod.__beamforming_serial_MOD_fortran_array_mult_sub.argtypes = [ctypes.POINTER(ctypes.c_float),ctypes.POINTER(ctypes.c_float),ctypes.POINTER(ctypes.c_float),ctypes.POINTER(ctypes.c_int)]
+    aa = np.random.rand(5).astype('float32')
+    ab = np.random.rand(5).astype('float32')
+    ao = np.zeros_like(aa)
+    caa = aa.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    cab = ab.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    cao = ao.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    c_float_p = ctypes.POINTER(ctypes.c_float)
+    mod.__beamforming_serial_MOD_fortran_array_mult_sub(caa,cab,cao,ctypes.pointer(ctypes.c_int(5)))
+    print(cao)
+    
+    #multiple math operations
+    mod.__beamforming_serial_MOD_fortran_array_math_sub.argtypes = [ctypes.POINTER(ctypes.c_float),ctypes.POINTER(ctypes.c_float),ctypes.POINTER(ctypes.c_float),ctypes.POINTER(ctypes.c_int)]
+    
+    n = 100000
+    arr1 = np.random.rand(n).astype('float32')
+    arr2 = np.random.rand(n).astype('float32')
+    
+    def test_mult_python(arr1,arr2):
+        '''
+        @brief timing with numpy iterating
+        '''
+        arro = np.zeros_like(arr1)
+        for i in range(len(arr1)):
+            arro[i] = arr1[i]*arr2[i]
+        return arro
+            
+    def test_mult_numpy(arr1,arr2):
+        arro = arr1*arr2
+        return arro
+    
+    def test_mult_fortran(arr1,arr2):
+        arro = np.zeros_like(arr1)
+        mod.__beamforming_serial_MOD_fortran_array_mult_sub(arr1.ctypes.data_as(c_float_p),arr2.ctypes.data_as(c_float_p),
+            arro.ctypes.data_as(c_float_p),ctypes.pointer(ctypes.c_int(arro.size)))
+        return arro
+    
+    carr1 = arr1.ctypes.data_as(c_float_p)
+    carr2 = arr2.ctypes.data_as(c_float_p)
+    arro = np.zeros_like(arr1); carro = arro.ctypes.data_as(c_float_p)
+    cn = ctypes.pointer(ctypes.c_int(n))
+    def test_mult_fortran_no_cast(carr1,carr2,carro,cn):
+        mod.__beamforming_serial_MOD_fortran_array_mult_sub(carr1,carr2,carro,cn)
+        #return carro
+    
+    def test_math_fortran_no_cast(carr1,carr2,carro,cn):
+        #This is faster in fortran! this shows that numpy is realy freakin fast, but only when the interpreter is not called
+        mod.__beamforming_serial_MOD_fortran_array_math_sub(carr1,carr2,carro,cn)
+        return carro
+        
+    def test_math_numpy(arr1,arr2):
+        arro = arr1*arr2
+        arro+=arr1
+        arro = arr2-arro
+        arro*=(arr1/arr2)
+        return arro
+    
+    from numba import jit
+    
+    @jit
+    def test_math_numba_jit(arr1,arr2):
+      #this is actulaly a little slower than numpy even (numpy calls some base level shenanigans)
+      arro = arr1*arr2
+      arro+=arr1
+      arro = arr2-arro
+      arro*=(arr1/arr2)
+      return arro  
+        
+        
+        
+        
+        
+        
     
     
     
--- beamforming/fortran/beamforming_serial.f90
+++ beamforming/fortran/beamforming_serial.f90
@@ -12,7 +12,7 @@
 MODULE BEAMFORMING_SERIAL
     !@brief module for defining serial beamforming
     USE beamforming_generic, ONLY: get_k
-    USE iso_c_bindings
+    !USE iso_c_bindings
     IMPLICIT NONE
 
 
@@ -28,9 +28,9 @@ MODULE BEAMFORMING_SERIAL
 
     END
 
-    FUNCTION fortran_mult_funct_c(test_num_a,test_num_b) BIND(C,name='fortran_mult_funct')
+    !FUNCTION fortran_mult_funct_c(test_num_a,test_num_b) BIND(C,name='fortran_mult_funct')
 
-    END
+    !END
 
 
     FUNCTION fortran_mult_funct(test_num_a,test_num_b)
@@ -49,38 +49,55 @@ MODULE BEAMFORMING_SERIAL
         out_arr = arr_a*arr_b
     END SUBROUTINE
 
-    FUNCTION get_partial_k_vector_azel(az,el)
-        ! @brief create a set of partial k vector (e.g. sin(theta)*cos(phi)) to be later
-        !   multiplied by k which will then be dotted with our position (x,y,z)
+    SUBROUTINE fortran_array_math_sub(arr_a,arr_b,out_arr,n)
+        ! @brief test python with fortran subroutines
+        INTEGER, intent(in) :: n
+        REAL, intent(in), DIMENSION(n) :: arr_a,arr_b
+        REAL, intent(inout), DIMENSION(n) :: out_arr 
+        out_arr = arr_a*arr_b
+        out_arr = arr_a+out_arr 
+        out_arr = arr_b-out_arr 
+        out_arr = out_arr*(arr_a/arr_b)
+    END SUBROUTINE
+
+    FUNCTION get_k_vector_azel(freq,az,el)
+        ! @brief create a set of k vectors (e.g. sin(theta)*cos(phi) - k_x,k_y,k_z) 
+        !   for a given frequency will then be dotted with our position (x,y,z)
         ! @note THIS IS CURRENTLY NOT USED
         ! @param[in] az - azimuth values
         ! @param[in] el - of elevation value 
         ! @param[out] partial_k_vector - array of shape (3,) (empty array should be passed in) with the partial k vectors for xyz
-        REAL, intent(in)                    :: az,el
-        REAL, DIMENSION(3)   :: get_partial_k_vector_azel
-        get_partial_k_vector_azel(1) = sin(az)*cos(el) ! x
-        get_partial_k_vector_azel(2) = sin(el)         ! y
-        get_partial_k_vector_azel(3) = cos(az)*cos(el) ! z
+        REAL, intent(in)                    :: az,el,freq
+        REAL                                :: k
+        REAL,              DIMENSION(3)     :: get_k_vector_azel
+        k = get_k(freq,1.,1.)
+        get_k_vector_azel(1) = sin(az)*cos(el) ! x
+        get_k_vector_azel(2) = sin(el)         ! y
+        get_k_vector_azel(3) = cos(az)*cos(el) ! z
+        get_k_vector_azel = k*get_k_vector_azel
     END
 
-    SUBROUTINE get_steering_vector(frequency,positions,az,el,steering_vectors)
-        ! @brief get our steering vectors at a given frequency, positinos, angles
+    SUBROUTINE get_steering_vectors_(frequency,positions,az,el,steering_vectors)
+        ! @brief get our steering vectors at a given frequency, positinos, angles. This will be wrapped by a iso_c function
         ! @param[in] frequency - frequency we are beamforming at
         ! @param[in] positions - Nx3 array of positions ((:,c) c=1,2,3 are x,y,z respectively
         ! @param[in] az - azimuth to get the steering vector for
         ! @param[in] el - elevation to calculate the steering vector for
-        ! @param[in] steering_vectors - empty complex array of length N to be filled for the steering vvectors of each element
+        ! @param[in] steering_vectors - empty complex array of shape (size(positions)(1),size(az))
         ! @note this can also be used to generate synthetic plane waves
-        REAL, intent(in) :: frequency, az, el
-        REAL, intent(in), DIMENSION(:,:) :: positions
-        COMPLEX, intent(out), DIMENSION(:) :: steering_vectors
-        REAL,DIMENSION(3) :: pkv,k !partial k vector,k value
-        pkv = get_partial_k_vector_azel(az,el)
-        k = get_k(frequency,1.,1.) ! get our wavenumber
-        steering_vectors = exp(CMPLX(0,-1)*matmul(positions,pkv*k)) ! e^(-j*dot(k,r))
+        REAL,   intent(in)                  :: frequency
+        REAL,   intent(in), DIMENSION(:)    :: az,el
+        REAL,   intent(in), DIMENSION(:,:)  :: positions
+        COMPLEX,intent(out),DIMENSION(:,:)  :: steering_vectors
+        REAL,               DIMENSION(3)    :: kvec !k vector for a given angle
+        INTEGER :: i
+        DO i=1,SIZE(az)
+            kvec = get_k_vector_azel(frequency,az(i),el(i))
+            steering_vectors(:,i) = exp(CMPLX(0,-1)*matmul(positions,kvec)) ! e^(-j*dot(k,r))
+        ENDDO
     END
 
-    COMPLEX FUNCTION get_beamformed_value(frequency,positions,weights,meas_vals,az,el)
+    SUBROUTINE get_beamformed_values_(freqs,positions,weights,meas_vals,az,el,out_vals)
         ! @brief get a beamformed value at a given freq, positions, and angles
         ! @param[in] frequency - frequency we are beamforming at
         ! @param[in] positions - Nx3 array of positions ((:,c) c=1,2,3 are x,y,z respectively
@@ -88,18 +105,25 @@ MODULE BEAMFORMING_SERIAL
         ! @param[in] meas_vals - complex measurements each of the elements (should be vector of length N)
         ! @param[in] az - azimuth to beamform at
         ! @param[in] el - elevation to beamform at
-        REAL, intent(in) :: frequency, az, el
-        REAL, intent(in), DIMENSION(:,:) :: positions
-        COMPLEX, intent(in), DIMENSION(:) :: weights, meas_vals
-        INTEGER, DIMENSION(2) :: num_positions
-        COMPLEX,ALLOCATABLE, DIMENSION(:) :: steering_vectors
+        ! @param[in] out_vals - output value matrix (:,n) is each frequency (n,:) is each az/el pair
+        REAL,    intent(in), DIMENSION(:)   :: freqs, az, el
+        REAL,    intent(in), DIMENSION(:,:) :: positions
+        COMPLEX, intent(in), DIMENSION(:)   :: weights 
+        COMPLEX, intent(in), DIMENSION(:,:) :: meas_vals ! meas_vals(:,n) is each frequency (n,:) is each az/el pair
+        INTEGER,             DIMENSION(2)   :: num_positions
+        COMPLEX,ALLOCATABLE, DIMENSION(:,:) :: steering_vectors
+        COMPLEX,intent(out), DIMENSION(:,:) :: out_vals !oru output values
+        INTEGER                             :: fn,an
         num_positions = shape(positions)
-        allocate(steering_vectors(num_positions(1)))
-        call get_steering_vector(frequency,positions,az,el,steering_vectors)
-        get_beamformed_value = SUM(weights*meas_vals*steering_vectors)
-        RETURN
-    END FUNCTION
-
+        allocate(steering_vectors(num_positions(1),size(az))) ! we need a steering vector for each xyz position triplet
+        DO fn=1,SIZE(freqs)
+            call get_steering_vectors_(freqs(fn),positions,az,el,steering_vectors) !get the steering vectors
+            DO an=1,size(az)
+                out_vals(an,fn) = SUM(weights*meas_vals(:,fn)*steering_vectors(:,an))
+            ENDDO
+        ENDDO
+        deallocate(steering_vectors)
+    END SUBROUTINE
 
 END MODULE BEAMFORMING_SERIAL
 
