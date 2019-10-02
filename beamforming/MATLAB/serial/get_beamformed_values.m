0b30b7e3d26c5c047c20b808c99225435df75e48
Alec <aweiss7791@gmail.com>
2019-10-02 16:44:42 -0600

    additions for matlab serial code

--- beamforming/MATLAB/serial/get_beamformed_values.m
+++ beamforming/MATLAB/serial/get_beamformed_values.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] = get_beamformed_values(inputArg1,inputArg2)
+ function [outputArg1,outputArg2] = get_beamformed_values(inputArg1,inputArg2)
 %GET_BEAMFORMED Summary of this function goes here
 %   Detailed explanation goes here
 outputArg1 = inputArg1;
--- beamforming/MATLAB/serial/get_steering_vectors.m
+++ beamforming/MATLAB/serial/get_steering_vectors.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] = get_steering_vectors(inputArg1,inputArg2)
+function [steering_vecs_out] = get_steering_vectors(inputArg1,inputArg2)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 outputArg1 = inputArg1;
