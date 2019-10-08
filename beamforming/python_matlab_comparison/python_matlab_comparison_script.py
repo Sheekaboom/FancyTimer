8f0ac720308a7c6fffa996864c03afbf0abacd71
Alec Weiss <aweiss@antenna-wireless-communications.local>
2019-10-08 13:55:15 -0600

    updates for matlab/python comparison

--- beamforming/python_matlab_comparison/python_matlab_comparison_script.py
+++ beamforming/python_matlab_comparison/python_matlab_comparison_script.py
@@ -9,13 +9,13 @@ from pycom.beamforming.python_interface.BeamformTest import fancy_timeit
 
 import numpy as np
 import scipy.linalg as linalg
-from scipy.linalg import lu as lu
+from scipy.linalg import lu
 import numba
 import pycuda
 
 m = 5000; n = 5000
-num_reps = 1
-dtype = np.double
+num_reps = 10
+dtype = np.cdouble
 a = np.random.rand(m,n)+1j*np.random.rand(m,n)
 b = np.random.rand(m,n)+1j*np.random.rand(m,n)
 a = a.astype(dtype)
@@ -46,17 +46,12 @@ Add/Sub/Mult/Div
 add_fun = lambda : np.add(a,b)
 [add_double_complex_time,add_double_complex_rv] = fancy_timeit(add_fun,num_reps);
 display_time_stats(add_double_complex_time,'Add Complex Double')
-'''
 sub_fun = lambda : np.subtract(a,b);
 [sub_double_complex_time,sub_double_complex_rv] = fancy_timer(sub_fun,num_reps);
 mul_fun = lambda : np.multiply(a,b);
 [mul_double_complex_time,mul_double_complex_rv] = fancy_timer(mul_fun,num_reps);
 div_fun = lambda : np.divide(a,b);
 [div_double_complex_time,div_double_complex_rv] = fancy_timer(div_fun,num_reps);
-'''
-
-
-
 
 '''
 exp/matmul/sum
@@ -65,6 +60,14 @@ matmul_fun = lambda : np.matmul(a,b)
 [matmul_double_complex_time,matmul_double_complex_rv] = fancy_timeit(matmul_fun,num_reps);
 display_time_stats(matmul_double_complex_time,'Matrix Multiply Complex Double')
 
+exp_fun = lambda : np.exp(a);
+[exp_double_complex_time,exp_double_complex_rv] = fancy_timeit(exp_fun,num_reps);
+display_time_stats(exp_double_complex_time,'Exponential Complex Double')
+
+sum_fun = lambda : np.sum(a);
+[sum_double_complex_time,sum_double_complex_rv] = fancy_timeit(sum_fun,num_reps);
+display_time_stats(sum_double_complex_time,'Sum Complex Double')
+
 
 '''
 LU Decomposition
@@ -72,6 +75,19 @@ LU Decomposition
 lu_fun = lambda : lu(a)
 [lu_double_complex_time,lu_double_complex_rv] = fancy_timeit(lu_fun,num_reps);
 display_time_stats(lu_double_complex_time,'LU Complex Double')
+
+'''
+FFT
+'''
+np.random.seed(1234)
+fft_n = 2**23; #number of points in our fft (same as matlab bench)
+fft_data = np.random.rand(fft_n)+1j*np.random.rand(fft_n);
+fft_data = fft_data.astype(dtype)
+fft_fun = lambda : np.fft.fft(fft_data);
+[fft_double_complex_time,fft_double_complex_rv] = fancy_timeit(fft_fun,num_reps);
+display_time_stats(fft_double_complex_time,'FFT Complex Double')
+
+
 
 
 
