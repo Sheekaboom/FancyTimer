83c3ff2174250668cb8b545ee9f17f8692edf16c
Alec <aweiss7791@gmail.com>
2019-12-23 10:27:44 -0700

    update test for matlab operationTimer and move scripts to google drive

--- base/TestOperationTimer.m
+++ base/TestOperationTimer.m
@@ -3,9 +3,9 @@
 %% Test the fancy_timeit_matrix_sweep capability
 mat_dim_list = 2.^(4:8);
 %mat_dim_list = np.floor(np.logspace(1,4,100)).astype(np.int32)
-funct_list   = {@plus,@minus,@times,@rdivide};
-funct_names  = {'add','sub' ,'mult','div'   };
-num_arg_list = [2     ,2    ,2     ,2       ];
+%funct_list   = {@plus,@minus,@times,@rdivide};
+%funct_names  = {'add','sub' ,'mult','div'   };
+%num_arg_list = [2     ,2    ,2     ,2       ];
 stats = OperationTimer.fancy_timeit_matrix_sweep(funct_list,funct_names,num_arg_list,mat_dim_list,100);
 
 % funct_list   = {@lu ,@mtimes  };
