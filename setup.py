06d1cfc167b0ae984d567ffe66ae323e3493aa3b
Sheekaboom <aweiss7791@gmail.com>
2020-01-27 19:22:51 -0700

    updates for package release

--- setup.py
+++ setup.py
@@ -4,23 +4,16 @@ Created on Tue Jan 21 10:37:05 2020
 
 @author: aweis
 """
+from setuptools import setup,find_packages
 
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
-from Cython.Compiler import Options
-
-
-
-extensions = [Extension('pycom.aoa.base.AoaAlgorithmCython',['./pycom/aoa/base/AoaAlgorithmCython.pyx']),
-              Extension('pycom.aoa.cython.CBFCython', ['./pycom/aoa/cython/CBFCython.pyx'])]
-
-Options.annotate = True
-
-setup(
-    ext_modules = cythonize(extensions)
-)
+setup(name='pycom',
+      version='0.1',
+      description='Python 5G communications. Thesis Code.',
+      author='Alec Weiss',
+      author_email='alec@weissworks.dev',
+      url='https://www.weissworks.dev',
+      packages=find_packages(),
+     )
 
 
-#build with `python setup.py build_ext --inplace` in anaconda prompt.
-#Befoer running this file needs to be moved one level outside of the package
+#build with `pip install -e .`
